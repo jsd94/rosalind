e96b8c2ff1ec30babb27a1bc23234e20f1f1aefd
John Sharp Decker <johnsharpdecker@gmail.com>
2022-03-08 14:42:33 -0500

    Added transcribe() to stringtools.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="rosalind_modules",
-    version="0.0.31",
+    version="0.0.314",
     description="",
     packages=setuptools.find_packages(),
     python_requires='>=3.7',
